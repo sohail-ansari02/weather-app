6d500c84b6ff6a761b6c5250cf3a95c4cae1f1e5
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-10-02 21:13:10 +0530

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TuiRootModule } from '@taiga-ui/core';
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './pages/weather/weather.component';
 import { ForecastComponent } from './pages/forecast/forecast.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent, WeatherComponent, ForecastComponent],
@@ -14,6 +15,12 @@ import { ForecastComponent } from './pages/forecast/forecast.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     TuiRootModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
