0d350b7a424777eae2884d6f3450145f3c60ba0e
Sohail-Fladdra <sohail.ansari@fladdra.com>
2023-10-02 20:54:57 +0530

    made weather ui page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './pages/weather/weather.component';
+import { ForecastComponent } from './pages/forecast/forecast.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/current-weather', pathMatch: 'full' }, // Redirect to the current weather page on the root URL
+  { path: 'current-weather', component: WeatherComponent },
+  { path: 'forecast', component: ForecastComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TuiRootModule } from '@taiga-ui/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './pages/weather/weather.component';
+import { ForecastComponent } from './pages/forecast/forecast.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WeatherComponent, ForecastComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
